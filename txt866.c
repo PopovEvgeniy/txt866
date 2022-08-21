e14d90483d94f55bf01f79f95e407ee7bed162aa
Popov Evgeniy <tuzik87@inbox.ru>
2022-08-21 17:47:31 +0700

    Small changes

--- txt866.c
+++ txt866.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 
 void show_intro();
-void command_line_help();
-void show_start_message();
-void show_end_message();
 FILE *open_input_file(const char *name);
 FILE *create_output_file(const char *name);
 int convert_code(const int target);
@@ -15,13 +12,13 @@ int main(int argc, char *argv[])
  show_intro();
  if (argc==3)
  {
-  show_start_message();
+  puts("Working.Please wait...");
   work(argv[1],argv[2]);
-  show_end_message();
+  puts("Work finish");
  }
  else
  {
-  command_line_help();
+  puts("You must give a input file name and output file name as command line arguments");
   exit(3);
  }
  return 0;
@@ -31,26 +28,10 @@ void show_intro()
 {
  putchar('\n');
  puts("TXT866");
- puts("Version 2.0.3");
+ puts("Version 2.0.5");
  puts("Win-1251 to Dos-866 code-page convertor for text files by Popov Evgeniy Alekseyevich,2010-2022 years");
  puts("Distributed under GNU GENERAL PUBLIC LICENSE");
  putchar('\n');
-}
-
-void command_line_help()
-{
- puts("You must give a input file name and output file name as command line arguments");
-}
-
-void show_start_message()
-{
- puts("Working.Please wait...");
-}
-
-void show_end_message()
-{
- putchar('\n');
- puts("Work finish");
 }
 
 FILE *open_input_file(const char *name)
@@ -59,7 +40,6 @@ FILE *open_input_file(const char *name)
  file=fopen(name,"rt");
  if (file==NULL)
  {
-  putchar('\n');
   puts("Can't open input file");
   exit(1);
  }
@@ -72,7 +52,6 @@ FILE *create_output_file(const char *name)
  file=fopen(name,"wt");
  if (file==NULL)
  {
-  putchar('\n');
   puts("Can't create output file");
   exit(2);
  }
